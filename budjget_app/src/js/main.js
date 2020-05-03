4e4ecd396c1311cfe1a235707161b04a295571d0
Dzmitry_Dauhaliavets <dolgolevets21@gmail.com>
2020-05-04 02:20:52 +0300

    add HomeTask #7

---
+++ budjget_app/src/js/main.js
@@ -0,0 +1,41 @@
+let startBtn = document.getElementById('start');
+
+let budgetValue = document.querySelector('.budget-value'),
+    dayBudgetValue = document.querySelector('.daybudget-value'),
+    levelValue = document.querySelector('.level-value'),
+    expensesValue = document.querySelector('.expenses-value'),
+    optionalExpensesValue = document.querySelector('.optionalexpenses-value'),
+    incomeValue = document.querySelector('.income-value'),
+    monthSavingsValue = document.querySelector('.monthsavings-value'),
+    yearSavingsValue = document.querySelector('.yearsavings-value');
+
+let expensesItem = document.getElementsByClassName('expenses-item');
+
+let optionalExpensesBtn = document.getElementsByTagName('button.optionalexpenses-btn'),
+    countBudgetBtn = document.getElementsByTagName('button.count-budget-btn');
+
+let optionalExpensesItem = document.querySelectorAll('.asideoptionalexpenses-item');
+
+let chooseIncome = document.querySelector('.choose-income'),
+    yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value'),
+    dayValue = document.querySelector('.day-value');
+
+console.log(startBtn);
+console.log(budgetValue);
+console.log(dayBudgetValue);
+console.log(levelValue);
+console.log(expensesValue);
+console.log(optionalExpensesValue);
+console.log(incomeValue);
+console.log(monthSavingsValue);
+console.log(yearSavingsValue);
+console.log(expensesItem);
+console.log(optionalExpensesBtn);
+console.log(countBudgetBtn);
+console.log(optionalExpensesItem);
+console.log(chooseIncome);
+console.log(yearValue);
+console.log(monthValue);
+console.log(dayValue);
+
