48aea117c35e536d8bb7defc4990fb21d31acef7
Dzmitry_Dauhaliavets <dolgolevets21@gmail.com>
2020-05-05 23:40:04 +0300

    timer added to project

--- yoga/scripts/scripts.js
+++ yoga/scripts/scripts.js
@@ -34,4 +34,62 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+    // =======================   Timer   =============================
+
+    let deadLine = '2020-05-06';
+
+    function getTimeRemsining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            hours = Math.floor((t / (1000 * 60 * 60)));
+
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds,
+        }
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemsining(endtime);
+            // Если полученные из t.hours, t.minutes и t.seconds значения
+            // меньше 10 (т.е. состоят из одной цифры), впереди передаваемого
+            // в .textContent значения подставляем строку '0'
+            if (t.hours < 10) {
+                hours.textContent = '0' + t.hours;
+            } else {
+                hours.textContent = t.hours;
+            }
+            if (t.minutes < 10) {
+                minutes.textContent = '0' + t.minutes;
+            } else {
+                minutes.textContent = t.minutes;
+            }
+            if (t.seconds < 10) {
+                seconds.textContent = '0' + t.seconds;
+            } else {
+                seconds.textContent = t.seconds;
+            }
+            // Если разница между deadline и текущей датой отрицательная,
+            // то передаем '00' и удаляет setInterval
+            if (t.total <= 0) {
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('timer', deadLine);
 });
